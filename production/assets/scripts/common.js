098f5e371788a639ee8c4d90ac0962acc4ca8e59
Devbrones <crhlm@pm.me>
2023-04-19 10:23:30 +0200

    redesigned everything, as one does

--- production/assets/scripts/common.js
+++ production/assets/scripts/common.js
@@ -1,32 +1,48 @@
-function generateNav() {
-    // get the nav element
-    var nav = document.getElementsByClassName('nav');
-    // get the nav items
-    var navItems = {% set navPages = collections.all | eleventyNavigation %}{{ navPages | dump | safe }};
-    // create list element in nav
-    var navList = document.createElement('ul');
-    // add class to list element
-    navList.classList.add('nav__list');
-    // append list element to nav
-    nav[0].appendChild(navList);
-    // loop through the nav items
-    for (var i = 0; i < navItems.length; i++) {
-        // create list item element
-        var navItem = document.createElement('li');
-        // add class to list item element
-        navItem.classList.add('nav__item');
-        // create link element
-        var navLink = document.createElement('a');
-        // add class to link element
-        navLink.classList.add('nav__link');
-        // add href to link element
-        navLink.setAttribute('href', navItems[i].url);
-        // add text to link element
-        navLink.innerHTML = navItems[i].title;
-        // append link element to list item element
-        navItem.appendChild(navLink);
-        // append list item element to list element
-        navList.appendChild(navItem);
-    }
+/*let mainNavLinks = document.querySelectorAll("nav ul li a");
+let mainSections = document.querySelectorAll("body container tape tape_content section");
+let lastId;
+let cur = [];
 
-}
+// This should probably be throttled.
+// Especially because it triggers during smooth scrolling.
+// https://lodash.com/docs/4.17.10#throttle
+// You could do like...
+// window.addEventListener("scroll", () => {
+// _.throttle(doThatStuff, 100);
+// });
+console.log(mainNavLinks);
+window.addEventListener("scroll", event => { 
+    let fromTop = window.scrollY; 
+    mainNavLinks.forEach(link => {
+        let section = document.querySelector(link.hash); 
+        if (section.offsetTop <= fromTop && section.offsetTop + section.offsetHeight > fromTop) { 
+            link.classList.add("current"); 
+        } else { 
+            link.classList.remove("current"); 
+        } 
+    }); 
+});
+*/
+// wait for the DOM to finish loading
+document.addEventListener('DOMContentLoaded', () => {
+const navLinks = document.querySelectorAll('nav ul li a');
+console.log(navLinks);
+window.addEventListener('scroll', () => {
+  const currentScrollPos = window.scrollY;
+  document.querySelectorAll('section').forEach((section) => {
+    const sectionTop = section.offsetTop;
+    const sectionHeight = section.clientHeight;
+    if (currentScrollPos >= sectionTop && currentScrollPos < sectionTop + sectionHeight) {
+        navLinks.forEach((link) => {
+            console.log(link.getAttribute('href'));
+            if (link.getAttribute('href') === `#${section.id}`) {
+                link.classList.add('current');
+            } else {
+                console.error('no match');
+                link.classList.remove('current');
+            }
+      });
+    }
+  });
+});
+});
